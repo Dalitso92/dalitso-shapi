95c3ad852c15f7e043d97a469afadd2b41bfa715
Dalitso92 <152139373+Dalitso92@users.noreply.github.com>
2025-02-03 10:59:11 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<style>
+		html,body{
+			width: 100%;
+			height: 100%;
+			margin: 0px;
+			padding: 0px;
+		}
+	</style>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="header and footer.css">
+	<link rel="stylesheet" type="text/css" href="main body.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<title>dalitso mwewa shapi</title>
+</head>
+<body>
+
+<header>
+<div class="logo">
+
+	<h2>Dalitso Shapi</h2>
+</div>
+	<nav>
+		<ul>
+			<li> <a href="#about">about</a></li>
+			<!--<li> <a href="#charge">services</a></li>-->
+			<li> <a href="#links">contacts</a></li>
+			
+		</ul>
+	</nav>
+
+
+</header>
+
+
+
+<div class="main">
+
+
+
+<img class="img" src="profile.jpg" alt="DALITSO SHAPI">
+
+
+
+<div class="para">
+
+
+	<h1>Dalitso shapi web developer.</h1>
+	
+<br>
+
+<h3 id="about">ABOUT</h3>
+<hr>
+
+<p>
+<h4>HI! my name is Dalitso mwewa Shapi</h4> 
+
+<p>
+	Im a webpage developer, a computer scientist in programming and music producer in FL studio software. If you want assistance with website i can assist you.
+</p>
+
+		<br>
+<!--	<p>
+		<h3 id="charge">CHARGES</h3>
+		<hr>
+
+		<ul>
+		<li>
+			single page website(static) k50.
+		</li>
+			multiple page websites k150 then k50 per page.	
+		</ul>
+		 
+
+	</p>-->
+
+<br>
+		<h3 id="resume">CONTANT INFORMATION</h3>
+		<hr>
+		<ul>
+			<li>
+				Phone numbers: 0978666213 or 0951361960
+			</li>
+			<li>
+				Gmail address:<a href="mailto:mwewashapi7792@gmail.com">shapidalitso@gmail.com</a>
+			</li>
+			<li >
+			   Download resume :<a href="shapiResume.docx" download>  <button>download</button></a>
+			</li>
+			<li >
+			   Download CV   :<a href="shapiCV.docx" download>  <button>download</button></a>
+			</li>
+
+		</ul>
+	
+
+
+
+</p>
+
+</div>
+
+<br>	
+<br>
+
+
+</div>
+
+<br>
+
+
+
+<footer>
+	
+
+	
+
+<br>
+	<ul>
+	    <li id="links"> <h3>SOCIAL LINKS</h3></li>
+	    <hr>
+<br>
+		<li>Facebook---<a href="https://www.facebook.com/profile.php?id=100075741066147">Dalitso Shapi
+			<i class="fa-brands fa-facebook"></i></a></li>
+<br>
+		<li>Audiomack---<a href="https://audiomack.com/ruuky92">Ruuky92 <i class="fa-solid fa-music"></i></a></li>	
+	</ul>
+<br>
+<h4>&copy; designed by Dalitso Mwewa Shapi 2025</h4>
+<br>
+<br>
+<h5>&copy; All RIGHTS RESERVED</h5>
+
+	  
+
+
+	
+</footer>
+
+</body>
+</html>
